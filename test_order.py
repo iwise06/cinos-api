4b732b8dc31e48d85f141f7978f48013d3664a9d
Isaac Wise <38229170+IsaacWise06@users.noreply.github.com>
2024-02-05 09:49:16 -0600

    Added tests for future functions

--- test_order.py
+++ test_order.py
@@ -4,6 +4,11 @@ from Drink import Drink
 
 
 class TestOrder(unittest.TestCase):
+    def test_get_total(self):
+        order = Order([Drink('water', ['lemon', 'cherry'], 'small'),
+                      Drink('water', ['lemon', 'cherry'], 'large')])
+        self.assertEqual(order.get_total(), 4.15)
+
     def test_get_items(self):
         order = Order([Drink('water', ['lemon', 'cherry'])])
         self.assertEqual(order.get_items(), [
@@ -14,21 +19,13 @@ class TestOrder(unittest.TestCase):
                       Drink('water', ['lemon', 'cherry'])])
         self.assertEqual(order.get_num_items(), 2)
 
-    def test_get_total(self):
-        order = Order([Drink('water', ['lemon', 'cherry']),
-                      Drink('water', ['lemon', 'cherry'])])
-        self.assertEqual(order.get_total(), 2)
-
     def test_add_item(self):
         order = Order([Drink('water', ['lemon', 'cherry'])])
         order.add_item(Drink('water', ['lemon', 'cherry']))
         self.assertEqual(
             order.get_items(), [
-                Drink(
-                    'water', [
-                        'lemon', 'cherry']), Drink(
-                    'water', [
-                        'lemon', 'cherry'])])
+                Drink('water', ['lemon', 'cherry']),
+                Drink('water', ['lemon', 'cherry'])])
 
     def test_remove_item(self):
         order = Order([Drink('water', ['lemon', 'cherry']),
