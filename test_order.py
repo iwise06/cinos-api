7cfdaf468e88b5901757f01d3b138f51119166cb
Isaac Wise <38229170+IsaacWise06@users.noreply.github.com>
2024-02-05 10:13:44 -0600

    Added receipt tests

--- test_order.py
+++ test_order.py
@@ -9,6 +9,26 @@ class TestOrder(unittest.TestCase):
                       Drink('water', ['lemon', 'cherry'], 'large')])
         self.assertEqual(order.get_total(), 4.15)
 
+    def test_get_receipt(self):
+        order = Order([Drink('water', ['lemon', 'cherry'], 'small'),
+                      Drink('water', ['lemon', 'cherry'], 'large')])
+        self.assertEqual(order.get_recepit(), {
+                        drink_amount: 2,
+                        subtotal: 4.15,
+                        tax: round(4.15 * .0725, 2),
+                        total: 4.15 + round(4.15 * .0725, 2),
+                        drinks: [
+                            {
+                                drink: order[0],
+                                cost: order[0].get_total()
+                            }, 
+                            {
+                                drink: order[1],
+                                cost: order[1].get_total()
+                            }
+                        ],
+                    })
+
     def test_get_items(self):
         order = Order([Drink('water', ['lemon', 'cherry'])])
         self.assertEqual(order.get_items(), [
@@ -23,9 +43,7 @@ class TestOrder(unittest.TestCase):
         order = Order([Drink('water', ['lemon', 'cherry'])])
         order.add_item(Drink('water', ['lemon', 'cherry']))
         self.assertEqual(
-            order.get_items(), [
-                Drink('water', ['lemon', 'cherry']),
-                Drink('water', ['lemon', 'cherry'])])
+            order.get_items(), [order[0], order[1]])
 
     def test_remove_item(self):
         order = Order([Drink('water', ['lemon', 'cherry']),
