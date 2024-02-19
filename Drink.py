abf587e201f123e77faf383817216a047353224e
Isaac Wise <iamisaacwise2006@gmail.com>
2024-02-18 23:17:39 -0600

    Docstrings for the food class

--- Drink.py
+++ Drink.py
@@ -1,6 +1,6 @@
 """Drink class that contains the base, flavors, size, and cost of a drink.
 
-This class is used to represent a drink in a drink order. It contains the
+This class is used to represent a drink in an order. It contains the
 base, flavors, size, and cost of the drink. It also contains methods to
 manipulate the flavors and size of the drink.
 
@@ -18,6 +18,7 @@ class Drink:
         size: A string representing the size of the drink.
         cost: A float representing the cost of the drink.
     """
+
     valid_bases = ['water', 'sbrite', 'pokeacola',
                    'Mr. Salt', 'Hill Fog', 'Leaf Wine']
     valid_flavors = ['lemon', 'cherry',
@@ -33,10 +34,12 @@ class Drink:
         """
         # Check if base is a list and raise an error if it has more than one
         # string
+
         if isinstance(base, list) and len(base) > 1:
             raise ValueError('Only one base is allowed')
 
         # Check if the base is an invalid base and raise error if it is
+
         if base not in self.valid_bases:
             raise ValueError(f'Invalid base: {base}')
 
@@ -52,20 +55,11 @@ class Drink:
 
     # Need to to implement __eq__ method to compare drinks since
     # the default __eq__ method compares the memory location of the objects
+
     def __eq__(self, other):
         if not isinstance(other, Drink):
             return False
         return self._base == other._base and self._flavors == other._flavors
-
-    def _calculate_cost(self):
-        """Calculates the cost of the drink based on the size and number of flavors."""
-        size_costs = {'small': 1.5, 'medium': 1.75,
-                      'large': 2.05, 'mega': 2.15}
-
-        return round(size_costs[self._size] + 0.15 * len(self._flavors), 2)
-
-    # Function to validate flavors and raise errors if needed
-    # Change this and use sets
 
     def _validate_flavors(self, flavors):
         """Validates the flavors of the drink and raises errors if needed."""
@@ -79,9 +73,12 @@ class Drink:
 
             unique_flavors.append(flavor)
 
-    def get_flavors(self):
-        """Returns the list of flavors of the drink."""
-        return self._flavors
+    def _calculate_cost(self):
+        """Calculates the cost of the drink based on the size and number of flavors."""
+        size_costs = {'small': 1.5, 'medium': 1.75,
+                      'large': 2.05, 'mega': 2.15}
+
+        return round(size_costs[self._size] + 0.15 * len(self._flavors), 2)
 
     def get_base(self):
         """Returns the base of the drink."""
@@ -95,7 +92,12 @@ class Drink:
         """Returns the number of flavors of the drink."""
         return len(self._flavors)
 
+    def get_flavors(self):
+        """Returns the list of flavors of the drink."""
+        return self._flavors
+
     def set_flavors(self, flavors):
+        """Sets the flavors of the drink."""
         self._validate_flavors(flavors)
         self._flavors = flavors
 
@@ -125,12 +127,3 @@ class Drink:
             raise ValueError(f'Invalid size: {size}')
         self._size = size
         self._cost = self._calculate_cost()
-
-    def to_dict(self):
-        """Returns a dictionary representation of the drink."""
-        return {
-            'base': self._base,
-            'flavors': self._flavors,
-            'size': self._size,
-            'cost': self._cost
-        }
--- Food.py
+++ Food.py
@@ -1,12 +1,39 @@
+"""Food class that contains the base, toppings, and cost of a food item.
+
+This class is used to represent a food item in an order. It contains the
+base, toppings, and cost of the food. It also contains methods to
+manipulate the toppings of the food.
+
+Typical usage example:
+    food = Food('hotdog', ['chili', 'nacho cheese'])
+"""
+
+
 class Food:
+    """Class that contains the base, toppings, and cost of a food item.
+
+    Attributes:
+        base: A string representing the base of the food item.
+        toppings: A list of strings representing the toppings of the food.
+        cost: A float representing the cost of the food.
+    """
+
     valid_base = ['hotdog', 'corndog', 'ice cream',
                   'onion rings', 'french fries', 'tater tots', 'nacho chips']
     valid_toppings = ['cherry', 'whipped cream', 'caramel sauce', 'chocolate sauce',
                       'nacho cheese', 'chili', 'bacon bits', 'ketchup', 'mustard']
 
     def __init__(self, base, toppings):
+        """Initializes the food with the base and toppings.
+
+        Args:
+            base: A string representing the base of the food.
+            toppings: A list of strings representing the toppings of the food.
+        """
         if isinstance(base, list) and len(base) > 1:
             raise ValueError('Only one base is allowed')
+
+        # Check if the base is an invalid base and raise error if it is
 
         if base not in self.valid_base:
             raise ValueError(f'Invalid base: {base}')
@@ -19,23 +46,26 @@ class Food:
 
     # Need to to implement __eq__ method to compare food since
     # the default __eq__ method compares the memory location of the objects
+
     def __eq__(self, other):
         if not isinstance(other, Food):
             return False
         return self._base == other._base and self._toppings == other._toppings
 
     def _validate_toppings(self, toppings):
+        """Validates the toppings of the food and raises errors if needed."""
         unique_toppings = []
 
-        for flavor in toppings:
-            if flavor not in self.valid_toppings:
-                raise ValueError(f'Invalid flavor: {flavor}')
-            if flavor in unique_toppings:
-                raise ValueError(f'Duplicate flavor: {flavor}')
+        for topping in toppings:
+            if topping not in self.valid_toppings:
+                raise ValueError(f'Invalid topping: {topping}')
+            if topping in unique_toppings:
+                raise ValueError(f'Duplicate topping: {topping}')
 
-            unique_toppings.append(flavor)
+            unique_toppings.append(topping)
 
     def _calculate_cost(self):
+        """Calculates the cost of the food based on the base and the toppings."""
         topping_costs = {'cherry': 0.0, 'whipped cream': 0.0,
                          'caramel sauce': 0.5, 'chocolate sauce': 0.5,
                          'nacho cheese': 0.3, 'chili': 0.6, 'bacon bits': 0.3,
@@ -48,24 +78,34 @@ class Food:
         return round(base_costs[self._base] + sum([topping_costs[topping] for topping in self._toppings]), 2)
 
     def get_base(self):
+        """Returns the base of the food."""
         return self._base
 
     def get_total(self):
+        """Returns the cost of the food."""
         return self._cost
 
     def get_num_toppings(self):
+        """Returns the number of toppings of the food."""
         return len(self._toppings)
 
     def get_toppings(self):
+        """Returns the list of toppings of the food."""
         return self._toppings
 
     def set_toppings(self, toppings):
+        """Sets the toppings of the food."""
         self._validate_toppings(toppings)
         self._toppings = toppings
 
         self._calculate_cost()
 
     def add_topping(self, topping):
+        """Adds a topping to the food.
+
+        Args:
+            topping: A string representing the topping to be added to the food.
+        """
         self._validate_toppings([topping])
         self._toppings.append(topping)
 
